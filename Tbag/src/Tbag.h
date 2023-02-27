d2a0e928b07a442c7307a948432a60edf718ed8b
Yanir Harel <yanir44321@gmail.com>
2023-02-27 15:15:57 +0200

    feat(): Base files

---
+++ Tbag/src/Tbag.h
@@ -0,0 +1,14 @@
+//
+// Created by Ofek on 27/02/2023.
+//
+
+#ifndef TBAG_TBAG_H
+#define TBAG_TBAG_H
+#include <math.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <ctype.h>
+#include <stdio.h>
+
+
+#endif //TBAG_TBAG_H
---
+++ Tbag/src/main.c
@@ -0,0 +1,10 @@
+//
+// Created by Ofek on 27/02/2023.
+//
+
+#include "Tbag.h"
+
+int main(int argc, char** argv, char** wenv) {
+    printf("nigga\n");
+    return 0;
+}
